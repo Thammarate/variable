c2ddefe30cb4a17601211aa2db3f6ae7f05e7614
Thammarate <122539714+Thammarate@users.noreply.github.com>
2023-01-26 08:45:42 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     Counter += 1
+    basic.showNumber(Counter)
 })
 input.onButtonPressed(Button.AB, function () {
     Counter = 0
+    basic.showNumber(Counter)
 })
 input.onButtonPressed(Button.B, function () {
     Counter += -1
+    basic.showNumber(Counter)
 })
 let Counter = 0
 Counter = 0
-basic.forever(function () {
-    basic.showNumber(Counter)
-})
+basic.showNumber(Counter)
